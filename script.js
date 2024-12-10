16716a18d13a5d80fff08d0ac291337f2ee88916
♡°𝓕𝓪𝓽𝓲𝓶𝓪°杀叛徒 <iamqasim221@gmail.com>
2024-12-11 01:12:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// typing animation 
+// var typed = new Typed (".typing",{
+//     Strings:["","Web Designer","Web Developer","UI/UX Designer"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// });
+var typed = new Typed('.typing', {
+    strings: [ "WEBSITE and","APP Developer.",],
+    typeSpeed: 100,
+    BackSpeed:30,
+    loop:true
+  });
+   function sendmess(){
+    location.href='thankypu-page.jpg'
+   }
